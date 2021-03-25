6974c57d4a3dd467f52181f768aeb1ec0633ce20
Serg Basin <shoom.pozitiv@gmail.com>
2021-03-25 19:28:12 +0200

    Initial Commit

---
+++ JSON Parsing Practice/JSON_Parsing_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSON_Parsing_PracticeApp.swift
+//  JSON Parsing Practice
+//
+//  Created by Sergey Basin on 25.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct JSON_Parsing_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
